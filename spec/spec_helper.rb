2ab66f7582e30c3db23fbde16ed07931ef1c90db
maeve <maeve.revels@getg5.com>
2014-01-31 20:01:38 +0000

    Create empty rspec-rails directories

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'capybara/rspec'
 
 Rails.backtrace_cleaner.remove_silencers!
 
