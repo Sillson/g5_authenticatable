9f861cdfb425a76423809ddb3eead4aa11340b9b
maeve <maeve.revels@getg5.com>
2014-02-03 23:24:33 +0000

    Add g5_access_token to user factory

--- spec/factories/g5_authenticatable_users.rb
+++ spec/factories/g5_authenticatable_users.rb
@@ -5,6 +5,7 @@ FactoryGirl.define do
     sequence(:email) { |n| "test.user#{n}@test.host" }
     provider 'g5'
     sequence(:uid) { |n| "abc123-#{n}" }
+    sequence(:g5_access_token) { |n| "secret_token_#{n}" }
     password 'test_secret'
   end
 end
