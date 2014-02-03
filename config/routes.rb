3bec7dbe9fdd24c4e206140383b3f4ec379773df
maeve <maeve.revels@getg5.com>
2014-02-03 19:17:25 +0000

    Set devise router_name for mountable engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 G5Authenticatable::Engine.routes.draw do
-  devise_for :users, :class_name => "G5Authenticatable::User"
-
+  devise_for :users, class_name: 'G5Authenticatable::User',
+                     module: :devise
 end
