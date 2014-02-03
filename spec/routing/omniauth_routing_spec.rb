b9309df5896be3f95024df7b56ea08d8eae42f50
maeve <maeve.revels@getg5.com>
2014-02-03 09:43:15 +0000

    Specs around omniauth routing

---
+++ spec/routing/omniauth_routing_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe 'OmniAuth' do
+  describe 'routing' do
+    it 'should route GET /users/auth/g5' do
+      expect(get '/users/auth/g5').to be_routable
+    end
+
+    it 'should route GET /users/auth/g5/callback' do
+      expect(get '/users/auth/g5/callback').to be_routable
+    end
+  end
+
+  describe 'route helpers' do
+    it 'should generate a user_g5_authorize_path' do
+      expect(user_g5_authorize_path).to eq('/users/auth/g5')
+    end
+
+    it 'should generate a user_g5_callback_path' do
+      expect(user_g5_callback_path).to eq('/users/auth/g5/callback')
+    end
+  end
+end
