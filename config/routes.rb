08dc2929d010a228012b7ccbb98e717779b352d1
maeve <maeve.revels@getg5.com>
2014-02-03 02:10:25 +0000

    rails generate devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,4 @@
 G5Authenticatable::Engine.routes.draw do
+  devise_for :users, :class_name => "G5Authenticatable::User"
+
 end
---
+++ spec/factories/g5_authenticatable_users.rb
@@ -0,0 +1,6 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :g5_authenticatable_user, :class => 'User' do
+  end
+end
