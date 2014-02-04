865f1bc03d292ea1bf4687720d36314e99fc69df
maeve <maeve.revels@getg5.com>
2014-02-04 21:26:54 +0000

    Remove orm hook from install generator for now

--- lib/generators/g5_authenticatable/install_generator.rb
+++ lib/generators/g5_authenticatable/install_generator.rb
@@ -4,13 +4,7 @@ module G5Authenticatable
   class InstallGenerator < Rails::Generators::Base
     namespace 'g5_authenticatable'
 
-    desc 'Creates a migration for the user model, copies the locale files,
-          and mounts the g5_authenticatable engine.'
-
-    hook_for :orm
-
-    def copy_locale
-    end
+    desc 'Installs the g5_authenticatable engine.'
 
     def mount_engine
       route "mount G5Authenticatable::Engine => '/g5_auth'"
