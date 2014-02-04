c3544a85e81658c9dc1820b37915f5c129aacd71
maeve <maeve.revels@getg5.com>
2014-02-04 00:10:37 +0000

    Use generic url helpers (non-route helpers)

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe ::ApplicationController do
+  xit 'should have the correct new_user_session_path' do
+    expect(controller.new_user_session_path).to eq('/g5_auth/users/sign_in')
+  end
+
+  it 'should have the correct new_session_path for users' do
+    expect(controller.new_session_path(:user)).to eq('/g5_auth/users/sign_in')
+  end
+
+  it 'should have the correct destroy_session_path for users' do
+    expect(controller.destroy_session_path(:user)).to eq('/g5_auth/users/sign_out')
+  end
+
+  it 'should have the correct g5_authorize_path for users' do
+    expect(controller.g5_authorize_path(:user)).to eq('/g5_auth/users/auth/g5')
+  end
+
+  it 'should have the correct g5_callback_path for users' do
+    expect(controller.g5_callback_path(:user)).to eq('/g5_auth/users/auth/g5/callback')
+  end
+end
--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -3,13 +3,19 @@ require 'spec_helper'
 describe 'Signing in' do
   let(:user) { create(:g5_authenticatable_user) }
 
-  before { visit_path_and_login_with(g5_authenticatable.new_user_session_path, user) }
+  context 'from a login link' do
+    before do
+      visit root_path
+      stub_g5_omniauth(user)
+      click_link 'Login'
+    end
 
-  it 'should sign in the user successfully' do
-    expect(page).to have_content('Signed in successfully.')
-  end
+    it 'should sign in the user successfully' do
+      expect(page).to have_content('Signed in successfully.')
+    end
 
-  it 'should redirect the user to the root path' do
-    expect(current_path).to eq(root_path)
+    it 'should redirect the user to the root path' do
+      expect(current_path).to eq(root_path)
+    end
   end
 end
