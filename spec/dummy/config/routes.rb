ed639665105f07f33808cbac485501a0a505aea1
maeve <maeve.revels@getg5.com>
2014-02-05 22:15:02 +0000

    Add http verb to protected page route

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resource :home, only: [:index, :show]
 
-  match '/protected_page', to: 'home#show', as: :protected_page
+  get '/protected_page', to: 'home#show', as: :protected_page
 
   root to: 'home#index'
 end
