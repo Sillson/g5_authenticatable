d954c9f120c3be9547d20d5af217a0db73807961
maeve <maeve.revels@getg5.com>
2014-12-17 20:57:43 +0000

    Allow sign out via get or delete

---
+++ spec/routing/sign_out_routing_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe 'Sign out routes' do
+  routes {G5Authenticatable::Engine.routes}
+
+  it 'should route GET /g5_auth/users/sign_out' do
+    expect(get('/users/sign_out')).to route_to(controller: 'g5_authenticatable/sessions',
+                                               action: 'destroy')
+  end
+
+  it 'should route DELETE /g5_auth/users/sign_out' do
+    expect(delete('/users/sign_out')).to route_to(controller: 'g5_authenticatable/sessions',
+                                                  action: 'destroy')
+
+  end
+end
