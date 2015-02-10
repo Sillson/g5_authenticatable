46adcd726eb838424ff237dfc1c60d2e94854ff9
maeve <maeve.revels@getg5.com>
2015-02-10 03:54:39 +0000

    Replace huge comment with link to github issue

--- app/controllers/g5_authenticatable/failure_app.rb
+++ app/controllers/g5_authenticatable/failure_app.rb
@@ -5,14 +5,8 @@ class G5Authenticatable::FailureApp < Devise::FailureApp
     opts[:format] = request_format unless skip_format?
 
     config = Rails.application.config
-    # In Devise::FailureApp, the following line is implemented as
-    # opts[:script_name] = (config.relative_url_root if config.respond_to?(:relative_url_root))
-    # However, in Rails 4.2, passing in {:script_name => nil} to a route helper will override all
-    # prefixing logic, including the engine namespace.
-    # In other words, g5_authenticatable.new_user_session_path always returns "/g5_auth/users/sign_in".
-    # In Rails <= 4.1, g5_authenticatable.new_user_session_path(script_name: nil) also returns 
-    # "/g5_auth/users/sign_in", but in Rails 4.2, it returns "/users/sign_in", which isn't
-    # actually a route.
+
+    # See https://github.com/G5/g5_authenticatable/issues/25
     opts[:script_name] = config.relative_url_root if config.try(:relative_url_root)
 
     context = send(Devise.available_router_name)
