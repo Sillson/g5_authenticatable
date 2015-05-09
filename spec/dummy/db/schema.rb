c9339d69df7b207004963bacdef8bda3b8c74cfd
maeve <maeve.revels@getg5.com>
2015-05-09 06:23:46 +0000

    Run post migration in dummy app

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150429212919) do
+ActiveRecord::Schema.define(version: 20150509061150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,5 +55,10 @@ ActiveRecord::Schema.define(version: 20150429212919) do
   end
 
   add_index "g5_authenticatable_users_roles", ["user_id", "role_id"], name: "index_g5_authenticatable_users_roles_on_user_id_and_role_id", using: :btree
+
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
