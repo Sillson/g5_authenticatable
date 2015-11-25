f7a1e5301ebab4070a1174cf79ea35167cb4c4fa
Victor Garro <vgarroj@gmail.com>
2015-11-25 16:33:48 -0600

    Adding Log Myself Back URL to the session, improved security on methods

--- app/services/g5_authenticatable/impersonate_sessionable.rb
+++ app/services/g5_authenticatable/impersonate_sessionable.rb
@@ -2,8 +2,6 @@ module G5Authenticatable
   module ImpersonateSessionable
     extend ActiveSupport::Concern
 
-    private
-
     def impersonation_user?
       impersonation_user.present?
     end
@@ -12,16 +10,23 @@ module G5Authenticatable
       user_by_uid(impersonate_admin_uid)
     end
 
-    def clear_impersonation_keys
-      clear_impersonated_admin_id
-      clear_impersonating_user_uid
-    end
-
     def user_to_impersonate
       user_by_uid(impersonating_user_uid)
     end
 
+    def clear_impersonation_keys
+      impersonate_admin_uid = nil
+      impersonating_user_uid = nil
+      impersonating_user_callback_url = nil
+    end
+
     # Checks if the user by param is able to impersonate the second user by param
+    def check_impersonation_access?(user_impersonating, resource)
+      able_to_impersonate?(user_impersonating, resource)
+    end
+
+    private
+
     def able_to_impersonate?(user_impersonating, resource)
       return false unless user_impersonating.present? && resource.present?
       if user_impersonating.has_role?(:super_admin) ||
@@ -41,6 +46,7 @@ module G5Authenticatable
     # Let's just keep this key secret. you know.. for security reasons.
     IMPERSONATE_SESSION_KEY = 'devise.g5_authenticatable.Xf1eP3hCjC'
     IMPERSONATING_USER_SESSION_KEY = 'devise.g5_authenticatable.Zk35J5adER'
+    IMPERSONATING_USER_SESSION_CALLBACK_URL = 'devise.g5_authenticatable.callcbak_url'
 
     def impersonate_admin_uid
       request.env['rack.session'][IMPERSONATE_SESSION_KEY]
@@ -66,12 +72,12 @@ module G5Authenticatable
       request.env['rack.session'] && request.env['rack.session'][IMPERSONATING_USER_SESSION_KEY].present?
     end
 
-    def clear_impersonated_admin_id
-      request.env['rack.session'][IMPERSONATE_SESSION_KEY] = nil
+    def impersonating_user_callback_url
+      request.env['rack.session'][IMPERSONATING_USER_SESSION_CALLBACK_URL] || ''
     end
 
-    def clear_impersonating_user_uid
-      request.env['rack.session'][IMPERSONATING_USER_SESSION_KEY] = nil
+    def impersonating_user_callback_url=(callback_url)
+      request.env['rack.session'][IMPERSONATING_USER_SESSION_CALLBACK_URL] = callback_url
     end
   end
 end
