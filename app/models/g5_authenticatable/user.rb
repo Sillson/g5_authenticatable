cfaa8d9de569ee61b89ed69e388cc72d9bbf7d16
maeve <maeve.revels@getg5.com>
2016-03-30 18:29:32 +0000

    Refactor user methods into devise_g5_authenticatable

--- app/models/g5_authenticatable/user.rb
+++ app/models/g5_authenticatable/user.rb
@@ -9,23 +9,14 @@ module G5Authenticatable
 
     GLOBAL_ROLE = 'GLOBAL'
 
-    def self.new_with_session(params, session)
-      user = super(params, session)
-      auth_data = session['omniauth.auth']
-
-      if auth_data
-        user.update_roles_from_auth(auth_data)
-      end
-      user
-    end
-
-    def self.find_and_update_for_g5_oauth(auth_data)
-      user = super(auth_data)
-      if user
-        user.update_roles_from_auth(auth_data)
-      end
-
-      user
+    def self.auth_attributes(auth_data)
+      super(auth_data).merge({
+        first_name: auth_data.info.first_name,
+        last_name: auth_data.info.last_name,
+        phone_number: auth_data.info.phone,
+        title: auth_data.extra.title,
+        organization_name: auth_data.extra.organization_name
+      })
     end
 
     def update_roles_from_auth(auth_data)
@@ -45,16 +36,6 @@ module G5Authenticatable
 
     def locations
       G5Updatable::LocationPolicy::Scope.new(self, G5Updatable::Location).resolve
-    end
-
-    def self.auth_attributes(auth_data)
-      super(auth_data).merge({
-        first_name: auth_data.info.first_name,
-        last_name: auth_data.info.last_name,
-        phone_number: auth_data.info.phone,
-        title: auth_data.extra.title,
-        organization_name: auth_data.extra.organization_name
-      })
     end
 
     private
